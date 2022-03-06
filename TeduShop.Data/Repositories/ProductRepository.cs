c022f2881c38178bd9a44aacbd69002226698bad
leechan712 <tranglc.it@gmail.com>
2022-03-06 16:10:01 +0700

    1

---
+++ TeduShop.Data/Repositories/ProductRepository.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TeduShop.Data.Infrastructure;
+using TeduShop.Model.Models;
+
+namespace TeduShop.Data.Repositories
+{
+    public interface IProductRepository
+    {
+
+    }
+
+    public class ProductRepository: RepositoryBase<Product>
+    {
+        public ProductRepository(IDbFactory dbFactory)
+            : base(dbFactory)
+        {
+
+        }
+    }
+}
