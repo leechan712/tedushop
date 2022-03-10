c8f11b18289cab97d51aabfae92c7669ca048e34
leechan712 <tranglc.it@gmail.com>
2022-03-10 08:37:44 +0700

    add identity for web

---
+++ TeduShop.Data/Migrations/202203091713488_AddDataUser.cs
@@ -0,0 +1,20 @@
+﻿namespace TeduShop.Data.Migrations
+{
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using System;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+    using TeduShop.Model.Models;
+
+    public partial class AddDataUser : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- TeduShop.Data/Migrations/Configuration.cs
+++ TeduShop.Data/Migrations/Configuration.cs
@@ -1,9 +1,12 @@
 ﻿namespace TeduShop.Data.Migrations
 {
+    using Microsoft.AspNet.Identity;
+    using Microsoft.AspNet.Identity.EntityFramework;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
+    using TeduShop.Model.Models;
 
     internal sealed class Configuration : DbMigrationsConfiguration<TeduShop.Data.TeduShopDbContext>
     {
@@ -18,6 +21,40 @@
 
             //  You can use the DbSet<T>.AddOrUpdate() helper extension method
             //  to avoid creating duplicate seed data.
+
+            CreateUser(context);
+        }
+
+        private void CreateUser(TeduShopDbContext context)
+        {
+            var manager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(new TeduShopDbContext()));
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(new TeduShopDbContext()));
+
+            var user = new ApplicationUser()
+            {
+                UserName = "tedu",
+                Email = "tedu.international@gmail.com",
+                EmailConfirmed = true,
+                BirthDay = DateTime.Now,
+                FullName = "Technology Education"
+
+            };
+            if (manager.Users.Count(x => x.UserName == "tedu") == 0)
+            {
+                manager.Create(user, "123456@");
+
+                if (!roleManager.Roles.Any())
+                {
+                    roleManager.Create(new IdentityRole { Name = "Admin" });
+                    roleManager.Create(new IdentityRole { Name = "User" });
+                }
+
+                var adminUser = manager.FindByEmail("tedu.international@gmail.com");
+
+                manager.AddToRoles(adminUser.Id, new string[] { "Admin", "User" });
+            }
+
         }
     }
 }
