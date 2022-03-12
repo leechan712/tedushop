7df898d3000d6ce6a4eb26fd3ffdbe4d542baae0
leechan712 <tranglc.it@gmail.com>
2022-03-12 16:26:59 +0700

    add filter status and format data in list

---
+++ TeduShop.Web/app/shared/filters/statusFilter.js
@@ -0,0 +1,12 @@
+﻿(function (app) {
+    app.filter('statusFilter', statusFilter);
+
+    function statusFilter() {
+        return function (input) {
+            if (input == true)
+                return 'Kích hoạt';
+            else
+                return 'Khóa';
+        }
+    }
+})(angular.module('tedushop.common'));
