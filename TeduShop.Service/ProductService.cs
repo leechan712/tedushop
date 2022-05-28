b841df9d9e446dc994003b8a3459e29461499daa
leechan712 <tranglc.it@gmail.com>
2022-05-28 20:44:52 +0700

    product detail and related products

--- TeduShop.Service/ProductService.cs
+++ TeduShop.Service/ProductService.cs
@@ -28,6 +28,8 @@ namespace TeduShop.Service
 
         IEnumerable<Product> Search(string keyword, int page, int pageSize, string sort, out int totalRow);
 
+        IEnumerable<Product> GetReatedProducts(int id, int top);
+
         IEnumerable<string> GetListProductByName(string name);
 
         Product GetById(int id);
@@ -198,6 +200,12 @@ namespace TeduShop.Service
             totalRow = query.Count();
 
             return query.Skip((page - 1) * pageSize).Take(pageSize);
+        }
+
+        public IEnumerable<Product> GetReatedProducts(int id, int top)
+        {
+            var product = _productRepository.GetSingleById(id);
+            return _productRepository.GetMulti(x => x.Status && x.ID != id && x.CategoryID == product.CategoryID).OrderByDescending(x => x.CreatedDate).Take(top);
         }
     }
 }
