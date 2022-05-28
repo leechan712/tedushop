23136bf31f1dedd95cbf7bc68ab0b552db32213b
leechan712 <tranglc.it@gmail.com>
2022-05-28 22:45:43 +0700

    list product by tag

--- TeduShop.Data/Repositories/ProductRepository.cs
+++ TeduShop.Data/Repositories/ProductRepository.cs
@@ -10,7 +10,7 @@ namespace TeduShop.Data.Repositories
 {
     public interface IProductRepository : IRepository<Product>
     {
-
+          IEnumerable<Product> GetListProductByTag(string tagId, int page, int pageSize, out int totalRow);
     }
 
     public class ProductRepository : RepositoryBase<Product>, IProductRepository
@@ -19,6 +19,18 @@ namespace TeduShop.Data.Repositories
             : base(dbFactory)
         {
 
+        }
+
+        public IEnumerable<Product> GetListProductByTag(string tagId, int page, int pageSize, out int totalRow)
+        {
+            var query = from p in DbContext.Products
+                        join pt in DbContext.ProductTags
+                        on p.ID equals pt.ProductID
+                        where pt.TagID == tagId
+                        select p;
+            totalRow = query.Count();
+
+            return query.OrderByDescending(x => x.CreatedDate).Skip((page - 1) * pageSize).Take(pageSize);
         }
     }
 }
